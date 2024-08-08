165dabac86b5bf58ab5ebf9da732d4f7afc76cfe
Rafael <rafael.r.mateus@gmail.com>
2024-08-08 13:53:46 -0300

    add pratica com if e switch

---
+++ Modulo 1/Desenvolvimento Dinamico/ifElse.js
@@ -0,0 +1,16 @@
+let pais = "Argentina";
+if (pais != "Brasil"){
+    console.log("Você é estrangeiro.")
+} else {
+    console.log("Você é brasileiro.")
+}
+
+let idade = 15;
+if (idade < 16){
+    console.log("Você não pode votar");
+} else if(idade < 18 || idade > 65){
+    console.log("Voto opcional");
+}else {
+    console.log("Voto obrigatório");
+}
+
---
+++ Modulo 1/Desenvolvimento Dinamico/switch.js
@@ -0,0 +1,15 @@
+let diaSemana = 5;
+switch(diaSemana){
+    case 1:
+        console.log("Hoje é domingo - Pode descansar.");
+        break
+    case 2:
+        console.log("Hoje é segunda-feira - Bora trabalhar."); 
+        break
+    case 6:
+        console.log("Hoje é sexta-feira - Meu Deus, isso não acaba?.");  
+        break  
+    default:
+        console.log("Hoje é folga?.");
+        break
+}
