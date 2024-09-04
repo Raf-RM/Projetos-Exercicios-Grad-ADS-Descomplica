8bd203da5902c851dbb398fb7961d5f046889e4c
Rafael <rafael.r.mateus@gmail.com>
2024-09-04 04:00:49 -0300

    Add Prática com DOM 1 - Desenvolvimento Dinânico

---
+++ Modulo 1/Desenvolvimento Dinamico/elementoDOM2script.js
@@ -0,0 +1,4 @@
+var a = window.prompt("Digite o primeiro número");
+var b = window.prompt("Digite o segundo número");
+var total = Number(a) + Number(b);
+window.alert(`A soma de ${a} e ${b} é ${total}`);
