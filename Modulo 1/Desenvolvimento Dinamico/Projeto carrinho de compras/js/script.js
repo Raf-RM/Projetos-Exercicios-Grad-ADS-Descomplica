0080140d1a9e5cc3040787473cb7241373b46471
Rafael <rafael.r.mateus@gmail.com>
2024-09-14 12:17:01 -0300

    Add Finalização carrinho de compras - Desenvolvimento Dinâmico

--- Modulo 1/Desenvolvimento Dinamico/Projeto carrinho de compras/js/script.js
+++ Modulo 1/Desenvolvimento Dinamico/Projeto carrinho de compras/js/script.js
@@ -13,5 +13,31 @@ function adicionarItem(item){
     quantidade.innerHTML = qtd[item];
 
     total.innerHTML = valorTotal[item].toFixed(2);
-    
+    valorCompra();
+}
+
+function removerItem(item){
+    if(qtd[item] > 0){
+        var quantidade = document.getElementById("quantidade" + item);
+        var total = document.getElementById("total" + item);
+
+        qtd[item] -= 1;
+
+        valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
+
+        quantidade.innerHTML = qtd[item];
+
+        total.innerHTML = valorTotal[item].toFixed(2);
+    }
+    valorCompra();
+}
+
+function valorCompra(){
+    var valorTotalCompra = document.getElementById("valorTotalCompra");
+    var valor = 0;
+    for(var i = 0; i < valorTotal.length; i++){
+        valor += valorTotal[i]; 
+    }
+
+    valorTotalCompra.innerHTML = valor.toFixed(2);
 }
