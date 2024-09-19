9a17f9dd9e61ef6a4fdd826de9f72f93bbd6f6af
Rafael <rafael.r.mateus@gmail.com>
2024-09-19 07:41:59 -0300

    Add Finalizando Projeto Atv Prática 14 - Desenvolvimento Dinâmico

--- Modulo 1/Desenvolvimento Dinamico/Atv Pratica 14/main.js
+++ Modulo 1/Desenvolvimento Dinamico/Atv Pratica 14/main.js
@@ -47,64 +47,70 @@ function addNewEmployee() {
 
     var func;
 
-    if(name == ""){
-        throw new Error("Erro! PCampos marcados com asterísco são obrigatórios.")
-    }
+    try{
 
-    if (position == "Gerente") {
-        func = new Gerente(name, age, position, department)
-    } else if (position == "Desenvolvedor") {
-        func = new Desenvolvedor(name, age, position, language)
-    }
+        if(name == "" || age =="" || position == "" ){
+            throw new Error("Erro! Campos marcados com asterísco são obrigatórios.")   
+        }
 
-    employees.push(func)
+        if (position == "Gerente") {
+            func = new Gerente(name, age, position, department)
+        } else if (position == "Desenvolvedor") {
+            func = new Desenvolvedor(name, age, position, language)
+        }
 
-    var newLi = `
-        <li class="list-group-item d-flex justify-content-between lh-sm" id="employee${listEmployees.children.length}" >
-            <div>
-                <h6 class="my-0">${func.nome}</h6>
-                <small class="text-body-secondary">${func.cargo}</small>
-                <span id="status" class="badge bg-danger rounded-pill">Inativo</span>
-                
-            </div>
-            <div>
-                <span id="btnWork" class="btn btn-primary" onclick="employees[${employees.length - 1}].trabalhar()">Trabalhar</span>
-                <span class="btn btn-success" onclick="employees[${employees.length - 1}].seApresentar()">Apresentar</span>
-            </div>
-        </li>
-    `
+        employees.push(func)
 
-    if (position == "Gerente") {
-        newLi = `
-        <li class="list-group-item d-flex justify-content-between lh-sm" id="employee${listEmployees.children.length}" >
-            <div>
-                <h6 class="my-0">${func.nome}</h6>
-                <small class="text-body-secondary">${func.cargo}</small>                
-            </div>
-            <div>
-                <span class="btn btn-primary" onclick="employees[${employees.length - 1}].gerenciar()">Gerenciar</span>
-                <span class="btn btn-primary" onclick="employees[${employees.length - 1}].trabalhar()">Trabalhar</span>
-                <span class="btn btn-success" onclick="employees[${employees.length - 1}].seApresentar()">Apresentar</span>
-            </div>
-        </li>
+        var newLi = `
+            <li class="list-group-item d-flex justify-content-between lh-sm" id="employee${listEmployees.children.length}" >
+                <div>
+                    <h6 class="my-0">${func.nome}</h6>
+                    <small class="text-body-secondary">${func.cargo}</small>
+                    <span id="status" class="badge bg-danger rounded-pill">Inativo</span>
+                    
+                </div>
+                <div>
+                    <span id="btnWork" class="btn btn-primary" onclick="employees[${employees.length - 1}].trabalhar()">Trabalhar</span>
+                    <span class="btn btn-success" onclick="employees[${employees.length - 1}].seApresentar()">Apresentar</span>
+                </div>
+            </li>
         `
-    } else if (position == "Desenvolvedor") {
-        newLi = `
-        <li class="list-group-item d-flex justify-content-between lh-sm" id="employee${listEmployees.children.length}" >
-            <div>
-                <h6 class="my-0">${func.nome}</h6>
-                <small class="text-body-secondary">${func.cargo}</small>                
-            </div>
-            <div>
-                <span class="btn btn-primary" onclick="employees[${employees.length - 1}].programar()">Gerenciar</span>
-                <span class="btn btn-primary" onclick="employees[${employees.length - 1}].trabalhar()">Trabalhar</span>
-                <span class="btn btn-success" onclick="employees[${employees.length - 1}].seApresentar()">Apresentar</span>
-            </div>
-        </li>
-        `
+    
+        if (position == "Gerente") {
+            newLi = `
+            <li class="list-group-item d-flex justify-content-between lh-sm" id="employee${listEmployees.children.length}" >
+                <div>
+                    <h6 class="my-0">${func.nome}</h6>
+                    <small class="text-body-secondary">${func.cargo}</small>                
+                </div>
+                <div>
+                    <span class="btn btn-primary" onclick="employees[${employees.length - 1}].gerenciar()">Gerenciar</span>
+                    <span class="btn btn-primary" onclick="employees[${employees.length - 1}].trabalhar()">Trabalhar</span>
+                    <span class="btn btn-success" onclick="employees[${employees.length - 1}].seApresentar()">Apresentar</span>
+                </div>
+            </li>
+            `
+        } else if (position == "Desenvolvedor") {
+            newLi = `
+            <li class="list-group-item d-flex justify-content-between lh-sm" id="employee${listEmployees.children.length}" >
+                <div>
+                    <h6 class="my-0">${func.nome}</h6>
+                    <small class="text-body-secondary">${func.cargo}</small>                
+                </div>
+                <div>
+                    <span class="btn btn-primary" onclick="employees[${employees.length - 1}].programar()">Programar</span>
+                    <span class="btn btn-primary" onclick="employees[${employees.length - 1}].trabalhar()">Trabalhar</span>
+                    <span class="btn btn-success" onclick="employees[${employees.length - 1}].seApresentar()">Apresentar</span>
+                </div>
+            </li>
+            `
+        }
+    
+        listEmployees.innerHTML += newLi
+    }catch(error){
+        alert(error.message)
     }
 
-    listEmployees.innerHTML += newLi
     event.preventDefault();
 }
 
